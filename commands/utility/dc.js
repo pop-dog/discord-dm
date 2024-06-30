7befd131dda9663f240f755bffb441536f5f06ba
William Myers <wmyers09@gmail.com>
2024-06-30 12:13:41 -0700

    Initial commit.

---
+++ commands/utility/dc.js
@@ -0,0 +1,42 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+const BASE_DC = {
+    'Very easy': 5,
+    'Easy': 10,
+    'Medium': 15,
+    'Hard': 20,
+    'Very hard': 25,
+    'Nearly impossible': 30
+};
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('dc')
+		.setDescription('Generates a difficulty class of the specified difficulty.')
+		.addStringOption(option =>
+			option.setName('difficulty')
+                .addChoices(Object.entries(BASE_DC).map(x => {
+                    return {name: x[0], value: x[0]}
+                }))
+				.setDescription('The relative difficulty of the task.')
+				.setRequired(true)),
+	async execute(interaction) {
+		const difficulty = interaction.options.getString('difficulty', true);
+
+		if (!difficulty) {
+			return interaction.reply(`You must specify a difficulty!`);
+		}
+
+        if (!BASE_DC[difficulty]) {
+            return interaction.reply(`There is no difficulty class with name \`${difficulty}\`!`);
+        }
+
+        // Get the base DC
+        const dc = BASE_DC[difficulty];
+        // Add a random modifier between -3 and 3
+        const modifier = Math.floor(Math.random() * 7) - 3;
+        const total = dc + modifier;
+
+        await interaction.reply(`The DC of this ${difficulty} task is: **${total}**.`);
+	},
+};
